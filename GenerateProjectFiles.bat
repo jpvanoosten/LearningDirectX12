c5c1f4cd29c90d25128e8541032fac773f7ecd09
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2017-08-24 16:07:49 +0200

    Adding initial files.

---
+++ GenerateProjectFiles.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+
+PUSHD %~dp0
+
+REM Update these lines if the currently installed version of Visual Studio is not 2017.
+SET CMAKE_GENERATOR="Visual Studio 15 2017 Win64"
+SET CMAKE_BINARY_DIR=build_vs2017
+
+MKDIR %CMAKE_BINARY_DIR% 2>NUL
+PUSHD %CMAKE_BINARY_DIR%
+cmake -G %CMAKE_GENERATOR% -Wno-dev "%~dp0"
+
+POPD
+POPD
