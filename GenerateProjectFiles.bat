c107e31cfdb9bd768587b78b35996be5c4406114
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2018-01-09 12:30:33 +0100

    Adding cmake 3.10.1 and updating GenerateProjectFiles.bat to use cmake redistributable.

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -3,13 +3,14 @@
 PUSHD %~dp0
 
 REM Update these lines if the currently installed version of Visual Studio is not 2017.
+SET CMAKE="%~dp0\Tools\cmake-3.10.1-win64-x64\bin\cmake.exe"
 SET CMAKE_GENERATOR="Visual Studio 15 2017 Win64"
 SET CMAKE_BINARY_DIR=build_vs2017
 
 MKDIR %CMAKE_BINARY_DIR% 2>NUL
 PUSHD %CMAKE_BINARY_DIR%
 
-cmake -G %CMAKE_GENERATOR% -Wno-dev "%~dp0"
+%CMAKE% -G %CMAKE_GENERATOR% -Wno-dev "%~dp0"
 
 IF ERRORLEVEL 1 (
     PAUSE
