d2d00a21c35d85fc718ce6fc90d91953b4852ee1
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2018-02-23 22:26:52 +0100

    Update shaders for tutorial.

--- Tutorial2/shaders/PixelShader.hlsl
+++ Tutorial2/shaders/PixelShader.hlsl
@@ -1,6 +1,6 @@
 struct PixelShaderInput
 {
-    float4 Color    : COLOR;
+	float4 Color    : COLOR;
 };
 
 float4 main( PixelShaderInput IN ) : SV_Target
--- Tutorial2/shaders/VertexShader.hlsl
+++ Tutorial2/shaders/VertexShader.hlsl
@@ -1,7 +1,9 @@
-cbuffer ModelViewProjectionCB : register(b0)
+struct ModelViewProjection
 {
     matrix MVP;
-}
+};
+
+ConstantBuffer<ModelViewProjection> ModelViewProjectionCB : register(b0);
 
 struct VertexPosColor
 {
@@ -11,7 +13,7 @@ struct VertexPosColor
 
 struct VertexShaderOutput
 {
-    float4 Color    : COLOR;
+	float4 Color    : COLOR;
     float4 Position : SV_Position;
 };
 
@@ -19,7 +21,7 @@ VertexShaderOutput main(VertexPosColor IN)
 {
     VertexShaderOutput OUT;
 
-    OUT.Position = mul(MVP, float4(IN.Position, 1.0f));
+    OUT.Position = mul(ModelViewProjectionCB.MVP, float4(IN.Position, 1.0f));
     OUT.Color = float4(IN.Color, 1.0f);
 
     return OUT;
