065bc67723d96935ef09d292dcbd1ef3632707cb
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2018-04-09 23:24:26 +0200

    Updated the ResourceStateTracker class. Still need to finish the CommandList class which uses the resource state tracker.

--- DX12Lib/inc/ResourceStateTracker.h
+++ DX12Lib/inc/ResourceStateTracker.h
@@ -16,9 +16,15 @@
 
 #include "d3dx12.h"
 
-#include <unordered_set>
+#include <mutex>
+#include <map>
+#include <unordered_map>
 #include <vector>
 
+// An array (vector) of resource barriers.
+using ResourceBarriers = std::vector<D3D12_RESOURCE_BARRIER>;
+
+class CommandList;
 
 class ResourceStateTracker
 {
@@ -26,48 +32,153 @@ public:
     ResourceStateTracker();
     virtual ~ResourceStateTracker();
 
-    
+    /**
+     * Push a resource barrier to the resource state tracker.
+     * 
+     * @param barrier The resource barrier to push to the state tracker.
+     * @param flushImmediately Force flush any resource barriers that have been pushed
+     * to the command list.
+     */
+    void PushResourceBarrier(const D3D12_RESOURCE_BARRIER& barrier);
+
+    /**
+     * Retrieve all of the pending resource barriers. This is used by the command
+     * list to 
+     */
+    const ResourceBarriers& GetPendingResourceBarriers() const
+    {
+        return m_PendingResourceBarriers;
+    }
+
+    /**
+     * Flush any pending split resource barriers to the command list.
+     */
+    void FlushPendingResourceBarriers(CommandList& commandList);
+
+    /**
+     * Flush any resource barriers that have been pushed to the resource state
+     * tracker.
+     */
+    void FlushResourceBarriers(CommandList& commandList);
+
+    /**
+     * Commit final resource states to the global resource state array (map)
+     * This must be called when the command list is closed.
+     */
+    void CommitFinalResourceStates();
+
+    /**
+     * Reset state tracking. This must be done when the command list is reset.
+     */
+    void Reset();
+
+    /**
+     * Add a resource with a given state to the global resource state array (map).
+     * This should be done when the resource is created for the first time.
+     */
+    static void AddGlobalResourceState(ID3D12Resource* resource, D3D12_RESOURCE_STATES state);
+
+    /**
+     * Remove a resource from the global resource state array (map).
+     * This should be done when the resource is destroyed.
+     */
+    static void RemoveGlobalResourceState(ID3D12Resource* resource);
+
 protected:
 
 private:
-    // The current state of a (sub)resource.
-    struct SubresourceState
+    // Pending resource transitions are committed before a command list
+    // is executed on the command queue. This guarantees that resources will
+    // be in the expected state at the beginning of a command list.
+    ResourceBarriers m_PendingResourceBarriers;
+
+    // Resource barriers that need to be committed to the command list.
+    ResourceBarriers m_ResourceBarriers;
+
+    // Tracks the state of a particular resource and all of its subresources.
+    struct ResourceState
     {
-        SubresourceState( ID3D12Resource* resource, UINT subresource, D3D12_RESOURCE_STATES state = D3D12_RESOURCE_STATE_COMMON )
-            : Resource(resource)
-            , Subresource(subresource)
-            , State(state)
+        // Initialize all of the subresources within a resource to the given state.
+        ResourceState(D3D12_RESOURCE_STATES state = D3D12_RESOURCE_STATE_COMMON)
+            : State(state)
         {}
 
-        bool operator==( const SubresourceState& other )
+        // Returns true if any subresource defines a state.
+        // If false, then all (sub)resources have the same state.
+        bool HasSubresourceState() const
         {
-            return Resource == other.Resource &&
-                (Subresource == D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES || other.Subresource == D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES || Subresource == other.Subresource);
+            return !SubresourceState.empty();
         }
 
-        ID3D12Resource* Resource;
-        UINT Subresource;
+        // Get the state of a (sub)resource within the resource.
+        // If the specified subresource is not found in the SubresourceState array (map)
+        // then the state of the resource (D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES) is
+        // returned.
+        D3D12_RESOURCE_STATES GetSubresourceState(UINT subresource) const
+        {
+            D3D12_RESOURCE_STATES state = State;
+            const auto iter = SubresourceState.find(subresource);
+            if (iter != SubresourceState.end())
+            {
+                state = iter->second;
+            }
+            return state;
+        }
+
+        // Set a subresource to a particular state.
+        void SetSubresourceState(UINT subresource, D3D12_RESOURCE_STATES state)
+        {
+            if (subresource == D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES)
+            {
+                SetState(state);
+            }
+            else
+            {
+                SubresourceState[subresource] = state;
+            }
+        }
+
+        // Provide array-like access to subresource states.
+        // If subresource is D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES then the 
+        // state of the resource is returned.
+        D3D12_RESOURCE_STATES& operator[](UINT subresource)
+        {
+            if (subresource == D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES)
+            {
+                SubresourceState.clear();
+                return State;
+            }
+            else
+            {
+                return SubresourceState[subresource];
+            }
+        }
+
+        // Set all subresources within a resource to the same state.
+        void SetState(D3D12_RESOURCE_STATES state)
+        {
+            State = state;
+            // Clear the subresource array (map) to indicate that all subresources
+            // have the same state.
+            SubresourceState.clear();
+        }
+
+        // If the SubresourceState array (map) is empty, then the State variable defines 
+        // the state of all of the subresources.
         D3D12_RESOURCE_STATES State;
+        std::map<UINT, D3D12_RESOURCE_STATES> SubresourceState;
     };
 
-    struct SubresourceStateHasher
-    {
-        // Provide a hashing function for SubresourceState.
-        // A SubresourceState object is uniquely identified by its resource pointer 
-        // and its subresource. Using only the resource pointer as a hash value may
-        // cause collisions in a std::set or std::unordered_set but these will 
-        // be resolved by the SubresourceState::operator== function.
-        size_t operator()(const SubresourceState& subresourceState)
-        {
-            return reinterpret_cast<size_t>(subresourceState.Resource);
-        }
-    };
+    using ResourceStateMap = std::unordered_map<ID3D12Resource*, ResourceState>;
 
-    // Pending resource transitions.
-    std::vector<D3D12_RESOURCE_BARRIER> m_PendingResourceBarriers;
+    // The final (last known state) of the resources within a command list.
+    // The final resource state is committed to the global resource state when the 
+    // command list is closed but before it is executed on the command queue.
+    ResourceStateMap m_FinalResourceState;
 
-    // Global (sub)resource state.
-    using GlobalResourceState = std::unordered_set<SubresourceState, SubresourceStateHasher>;
-    static GlobalResourceState ms_GlobalResourceState;
-
+    // The mutex protects shared access to the GlobalResourceState map.
+    static std::mutex ms_GlobalMutex;
+    // The global resource state array (map) stores the state of a resource
+    // between command list execution.
+    static ResourceStateTracker::ResourceStateMap ms_GlobalResourceState;
 };
--- DX12Lib/src/ResourceStateTracker.cpp
+++ DX12Lib/src/ResourceStateTracker.cpp
@@ -2,11 +2,121 @@
 
 #include <ResourceStateTracker.h>
 
-// Initialize statics.
-ResourceStateTracker::GlobalResourceState ResourceStateTracker::ms_GlobalResourceState;
+#include <CommandList.h>
+
+// Static definitions.
+std::mutex ResourceStateTracker::ms_GlobalMutex;
+ResourceStateTracker::ResourceStateMap ResourceStateTracker::ms_GlobalResourceState;
 
 ResourceStateTracker::ResourceStateTracker()
 {}
 
 ResourceStateTracker::~ResourceStateTracker()
 {}
+
+void ResourceStateTracker::PushResourceBarrier(const D3D12_RESOURCE_BARRIER& barrier)
+{
+    if (barrier.Type == D3D12_RESOURCE_BARRIER_TYPE_TRANSITION)
+    {
+        const D3D12_RESOURCE_TRANSITION_BARRIER& transitionBarrier = barrier.Transition;
+
+        // First check if there is already a known "final" state for the given resource.
+        // If there is, the resource has been used on the command list before and
+        // already has a known state within the command list execution.
+        const auto iter = m_FinalResourceState.find(transitionBarrier.pResource);
+        if (iter != m_FinalResourceState.end())
+        {
+            // If the known final state of the resource is different...
+            auto& finalState = iter->second;
+            if (finalState[transitionBarrier.Subresource] != transitionBarrier.StateAfter )
+            {
+                // Push a new transition barrier with the correct before state.
+                D3D12_RESOURCE_BARRIER newBarrier = barrier;
+                newBarrier.Transition.StateBefore = finalState[transitionBarrier.Subresource];
+                m_ResourceBarriers.push_back(newBarrier);
+            }
+        }
+        else // In this case, the resource is being used on the command list for the first time...
+        {
+            // Check to see if there is a known global state for the resource.
+            std::lock_guard<std::mutex> lock(ms_GlobalMutex);
+            const auto iter = ms_GlobalResourceState.find(transitionBarrier.pResource);
+            if (iter != ms_GlobalResourceState.end())
+            {
+                // If the global state of the (sub) resource is different...
+                auto& globalState = iter->second;
+                if (globalState[transitionBarrier.Subresource] != transitionBarrier.StateAfter)
+                {
+                    // Create a pending barrier to begin the transition.
+                    D3D12_RESOURCE_BARRIER pendingBarrier = barrier;
+                    pendingBarrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_BEGIN_ONLY;
+                    pendingBarrier.Transition.StateBefore = globalState[transitionBarrier.Subresource];
+                    m_PendingResourceBarriers.push_back(pendingBarrier);
+                    pendingBarrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_END_ONLY;
+                    m_ResourceBarriers.push_back(pendingBarrier);
+                }
+            }
+            else
+            {
+                // This shouldn't happen. Resource must be added to the global resource state
+                // on resource creation. Resources should only be removed from the global resource state
+                // array on resource destruction.
+                throw std::invalid_argument("Resources must be registered using the ResourceStateTracker::AddGlobalResourceState method. Are you using the CommandList class to create and bind resources?");
+            }
+        }
+
+        // Push the final known state (possibly replacing the previously known state).
+        m_FinalResourceState[transitionBarrier.pResource][transitionBarrier.Subresource] = transitionBarrier.StateAfter;
+    }
+    else
+    {
+        // Just push the barrier to the resource barriers array.
+        m_ResourceBarriers.push_back(barrier);
+    }
+}
+
+void ResourceStateTracker::FlushResourceBarriers(CommandList& commandList)
+{
+    // TODO: Call ID3D12GraphicsCommandList::ResourceBarrier method with m_ResourceBarriers
+}
+
+void ResourceStateTracker::FlushPendingResourceBarriers(CommandList& commandList)
+{
+    // TODO: Call ID3D12GraphicsCommandList::ResourceBarrier method with m_PendingResourceBarriers
+}
+
+void ResourceStateTracker::CommitFinalResourceStates()
+{
+    // Commit final resource states to the global resource state array (map).
+    std::lock_guard<std::mutex> lock(ms_GlobalMutex);
+    for (const auto& resourceState : m_FinalResourceState)
+    {
+        ID3D12Resource* resource = resourceState.first;
+        for (const auto& subresourceState : resourceState.second.SubresourceState)
+        {
+            UINT subresource = subresourceState.first;
+            D3D12_RESOURCE_STATES state = subresourceState.second;
+            ms_GlobalResourceState[resource][subresource] = state;
+        }
+    }
+}
+
+void ResourceStateTracker::Reset()
+{
+    // Reset the pending, current, and final resource states.
+    m_PendingResourceBarriers.clear();
+    m_ResourceBarriers.clear();
+    m_FinalResourceState.clear();
+}
+
+void ResourceStateTracker::AddGlobalResourceState(ID3D12Resource* resource, D3D12_RESOURCE_STATES state)
+{
+    std::lock_guard<std::mutex> lock(ms_GlobalMutex);
+    ms_GlobalResourceState[resource][D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES] = state;
+}
+
+void ResourceStateTracker::RemoveGlobalResourceState(ID3D12Resource* resource)
+{
+    std::lock_guard<std::mutex> lock(ms_GlobalMutex);
+    ms_GlobalResourceState.erase(resource);
+}
