c562947f5a2b3fe41e43d846686eb41c3ab70777
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2018-09-19 09:44:20 +0200

    Allow the ImGui demo window to be hidden (Tutorial3).

--- Tutorial3/src/Tutorial3.cpp
+++ Tutorial3/src/Tutorial3.cpp
@@ -579,7 +579,10 @@ void Tutorial3::OnRender( RenderEventArgs& e )
     commandQueue->ExecuteCommandList( commandList );
 
     static bool showDemoWindow = true;
-    ImGui::ShowDemoWindow(&showDemoWindow);
+	if ( showDemoWindow )
+	{
+		ImGui::ShowDemoWindow(&showDemoWindow);
+	}
 
     // Present
     m_pWindow->Present( m_RenderTarget.GetTexture(AttachmentPoint::Color0) );
