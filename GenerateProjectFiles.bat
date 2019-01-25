1cd108c7f4a34647e1d4b355b24daad94c844bd6
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2019-01-25 15:37:35 +0100

    Updating to CMake 3.13.3.

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -3,7 +3,7 @@
 PUSHD %~dp0
 
 REM Update these lines if the currently installed version of Visual Studio is not 2017.
-SET CMAKE="%~dp0\Tools\cmake-3.12.3-win64-x64\bin\cmake.exe"
+SET CMAKE="%~dp0\Tools\cmake-3.13.3-win64-x64\bin\cmake.exe"
 SET CMAKE_GENERATOR="Visual Studio 15 2017 Win64"
 SET CMAKE_BINARY_DIR=build_vs2017
 
