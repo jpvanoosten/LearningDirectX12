99b8204380669d412327fab560b498db15725b61
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2019-01-26 17:29:04 +0100

    Fixed issue with GenerateProjectFiles.bat
    Update the GenerateProjectFiles.bat script file to detect Visual Studio 2019.
    NOTE: This is broken in Visual Studio 2019 Update 2 but will be fixed in the next release of CMake 3.14.0 (I hope).

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -7,26 +7,37 @@ SET VSWHERE="%~dp0\Tools\vswhere\vswhere.exe"
 SET CMAKE="%~dp0\Tools\cmake-3.13.3-win64-x64\bin\cmake.exe"
 
 REM Detect latest version of Visual Studio.
-FOR /F "delims=." %%i IN ('%VSWHERE% -latest -prerelease -requires Microsoft.VisualStudio.Workload.NativeGame -property installationVersion') DO (
+FOR /F "usebackq delims=." %%i IN (`%VSWHERE% -latest -prerelease -requires Microsoft.VisualStudio.Workload.NativeGame -property installationVersion`) DO (
     SET VS_VERSION=%%i
-	IF %VS_VERSION% == 16 (
-        SET CMAKE_GENERATOR="Visual Studio 16 2019 Win64"
-        SET CMAKE_BINARY_DIR=build_vs2019	
-	) ELSE IF %VS_VERSION% == 15 (
-        SET CMAKE_GENERATOR="Visual Studio 15 2017 Win64"
-        SET CMAKE_BINARY_DIR=build_vs2017
-	) ELSE IF %VS_VERSION% == 14 (
-        SET CMAKE_GENERATOR="Visual Studio 14 2015 Win64"
-        SET CMAKE_BINARY_DIR=build_vs2015
-    )
 )
 
-IF %VS_VERSION% LSS 14 (
-    ECHO This project requires at least Visual Studio 
-    ECHO Please use the CMake GUI application to configure your build system.
+IF %VS_VERSION% == 16 (
+    SET CMAKE_GENERATOR="Visual Studio 15 2017 Win64"
+    SET CMAKE_BINARY_DIR=build_vs2019	
+) ELSE IF %VS_VERSION% == 15 (
+    SET CMAKE_GENERATOR="Visual Studio 15 2017 Win64"
+    SET CMAKE_BINARY_DIR=build_vs2017
+) ELSE IF %VS_VERSION% == 14 (
+    SET CMAKE_GENERATOR="Visual Studio 14 2015 Win64"
+    SET CMAKE_BINARY_DIR=build_vs2015
+) ELSE (
+    ECHO.
+    ECHO ***********************************************************************
+    ECHO *                                                                     *
+    ECHO *                                ERROR                                *
+    ECHO *                                                                     *
+    ECHO ***********************************************************************
+    ECHO No compatible version of Microsoft Visual Studio detected.
+    ECHO Please make sure you have Visual Studio 2015 ^(or newer^) and the 
+    ECHO "Game Development with C++" workload installed before running this script.
+    ECHO. 
     PAUSE
     GOTO :Exit
 )
+
+ECHO CMake Generator: %CMAKE_GENERATOR%
+ECHO CMake Binary Directory: %CMAKE_BINARY_DIR%
+ECHO.
 
 MKDIR %CMAKE_BINARY_DIR% 2>NUL
 PUSHD %CMAKE_BINARY_DIR%
